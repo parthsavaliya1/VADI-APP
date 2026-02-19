598e3e943dd0b7a83b3c251d0ebea5face615de6
Parth Savaliya <parth.savaliya.ps1@gmail.com>
2026-02-19 16:26:15 +0530

    imprive design

--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-  // baseURL: "https://vadi-backend.onrender.com",
-  baseURL: "http://192.168.29.55:5000",
+  baseURL: "https://vadi-backend.onrender.com",
+  // baseURL: "http://192.168.29.55:5000",
 });
