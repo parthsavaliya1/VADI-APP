00623bfb6bc4ebcc22876c5b688709cdb98d1d64
Parth Savaliya <parth.savaliya.ps1@gmail.com>
2026-02-19 14:26:46 +0530

    change some code

--- utils/api.ts
+++ utils/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://vadi-backend.onrender.com",
+  // baseURL: "https://vadi-backend.onrender.com",
+  baseURL: "http://192.168.29.55:5000",
 });
