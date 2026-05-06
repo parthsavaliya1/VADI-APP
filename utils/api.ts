db360cb994873a20c5d1bec1f8b36538a6b32906
Parth Savaliya <parth.savaliya.ps1@gmail.com>
2026-05-06 14:04:00 +0530

    push

--- utils/api.ts
+++ utils/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const API = axios.create({
   // baseURL: "https://vadi-backend.onrender.com",
-  baseURL: "http://192.168.1.8:5000",
+  baseURL: "http://192.168.1.9:5000",
 });
---
+++ utils/pushNotifications.ts
@@ -0,0 +1,43 @@
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import { Platform } from "react-native";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
+export async function getPushToken() {
+  if (!Device.isDevice) {
+    return null;
+  }
+
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+
+  if (existingStatus !== "granted") {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+
+  if (finalStatus !== "granted") {
+    return null;
+  }
+
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  const tokenData = await Notifications.getDevicePushTokenAsync();
+  return tokenData?.data ? String(tokenData.data) : null;
+}
